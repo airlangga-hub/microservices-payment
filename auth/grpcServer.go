cfba369c9602ad40f2a6ccfe02e4d5210bbe1fc8
Airlangga <airlanggakrishna@gmail.com>
2026-01-03 01:59:38 +0700

    feat: NewServer

--- auth/grpcServer.go
+++ auth/grpcServer.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"context"
+	"database/sql"
 
 	"github.com/airlangga-hub/microservices-payment/auth/pb"
 )
 
 type Server struct {
 	pb.UnimplementedAuthServiceServer
+	db *sql.DB
+}
+
+func NewServer(db *sql.DB) *Server {
+	return &Server{db: db}
 }
 
 func (s *Server) GetToken(ctx context.Context, credentials *pb.Credentials) (*pb.Token, error)
