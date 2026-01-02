d9ccf8e50bc4794c00875899de43c53b3a18efc0
Airlangga <airlanggakrishna@gmail.com>
2026-01-03 02:40:27 +0700

    feat: init.sql and dto

--- auth/init.sql
+++ auth/init.sql
@@ -1,5 +1,23 @@
 CREATE USER 'auth_user'@'localhost' IDENTIFIED BY 'password';
 
-CREATE DATABASE IF NOT EXISTS users;
+CREATE DATABASE IF NOT EXISTS auth;
 
-GRANT ALL PRIVILEGES ON users.* TO 'auth_user'@'localhost';
+GRANT ALL PRIVILEGES ON auth.* TO 'auth_user'@'localhost';
+
+USE auth;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+INSERT INTO users (
+    email,
+    password
+)
+VALUES
+(
+    'user1@email.com',
+    'password123'
+);
