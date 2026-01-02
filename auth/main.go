090485dc23c400922ccf0e12ae1022b8ccb02ee9
Airlangga <airlanggakrishna@gmail.com>
2026-01-03 00:43:43 +0700

    feat: auth

--- auth/main.go
+++ auth/main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const (
+	dbDriver = "mysql"
+	
+	dbUser = "root"
+	dbPassword = "password"
+	
+	dbName = "users"
+)
+
+func main() {
+	dsn := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s", dbUser, dbPassword, dbName)
+	
+	db, err := sql.Open(dbDriver, dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Println("Error closing db: ", err)
+		}
+	}()
+	
+	if err := db.Ping(); err != nil {
+		log.Fatalln(err)
+	}
+}
