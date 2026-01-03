353a8202787e229fb1815481b338bf44ee52cbc6
Airlangga <airlanggakrishna@gmail.com>
2026-01-04 04:03:08 +0700

    feat: money movement helper

--- money_movement/go.mod
+++ money_movement/go.mod
@@ -2,12 +2,15 @@ module github.com/airlangga-hub/microservices-payment/money_movement
 
 go 1.25.3
 
-require google.golang.org/grpc v1.78.0
+require (
+	github.com/google/uuid v1.6.0
+	google.golang.org/grpc v1.78.0
+	google.golang.org/protobuf v1.36.10
+)
 
 require (
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
-	google.golang.org/protobuf v1.36.10 // indirect
 )
--- money_movement/helper.go
+++ money_movement/helper.go
@@ -1,6 +1,12 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
 
 func GetWallet(tx *sql.Tx, userID string) (Wallet, error) {
 
@@ -54,4 +60,114 @@ func GetAccount(tx *sql.Tx, walletID int32, accountType string) (Account, error)
 	}
 
 	return a, nil
+}
+
+func Transfer(tx *sql.Tx, srcAccount, dstAccount Account, amount int64) error {
+
+	if srcAccount.Cents < int32(amount) {
+		return status.Error(codes.InvalidArgument, "not enough money")
+	}
+
+	stmt, err := tx.Prepare(
+		`
+		UPDATE accounts
+		SET cents = ?
+		WHERE
+			id = ?;
+		`,
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(srcAccount.Cents-int32(amount), srcAccount.ID)
+	if err != nil {
+		return err
+	}
+
+	_, err = stmt.Exec(dstAccount.Cents+int32(amount), dstAccount.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+const (
+	insertTransactionQuery = `
+		INSERT INTO transactions (
+			pid,
+			src_user_id,
+			dst_user_id,
+			src_wallet_id,
+			dst_wallet_id,
+			src_account_id,
+			dst_account_id,
+			src_account_type,
+			dst_account_type,
+			final_dst_merchant_wallet_id,
+			amount
+		)
+		VALUES (
+			?,
+			?,
+			?,
+			?,
+			?,
+			?,
+			?,
+			?,
+			?,
+			?,
+			?
+		);
+	`
+	
+	selectTransactionQuery = `
+		SELECT
+			id,
+			pid,
+			src_user_id,
+			dst_user_id,
+			src_wallet_id,
+			dst_wallet_id,
+			src_account_id,
+			dst_account_id,
+			src_account_type,
+			dst_account_type,
+			final_dst_merchant_wallet_id,
+			amount
+		FROM transactions
+		WHERE
+			pid = ?;
+	`
+)
+
+func CreateTransaction(tx *sql.Tx, srcAccount, dstAccount Account, customerWallet, merchantWallet Wallet, amount int64) (string, error) {
+
+	pid := uuid.NewString()
+	
+	stmt, err := tx.Prepare(insertTransactionQuery)
+	if err != nil {
+		return "", err
+	}
+	
+	_, err = stmt.Exec(
+		pid,
+		customerWallet.UserID,
+		customerWallet.UserID,
+		srcAccount.WalletID,
+		dstAccount.WalletID,
+		srcAccount.ID,
+		dstAccount.ID,
+		srcAccount.AccountType,
+		dstAccount.AccountType,
+		merchantWallet.ID,
+		int32(amount),
+	)
+	if err != nil {
+		return "", err
+	}
+	
+	return pid, nil
 }
