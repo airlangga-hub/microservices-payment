6242a0018dfafead551b9d95a0b958c93dc84115
Airlangga <airlanggakrishna@gmail.com>
2026-01-04 00:21:59 +0700

    chore: main and pb

--- money_movement/main.go
+++ money_movement/main.go
@@ -1,1 +1,54 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/airlangga-hub/microservices-payment/money_movement/pb"
+	"google.golang.org/grpc"
+)
+
+const (
+	dbDriver = "mysql"
+
+	dbUser     = "auth_user"
+	dbPassword = "password"
+
+	dbName = "auth"
+)
+
+func main() {
+	dsn := fmt.Sprintf("%s:%s@tcp(localhost:3306)/%s", dbUser, dbPassword, dbName)
+
+	db, err := sql.Open(dbDriver, dsn)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Println("Error closing db: ", err)
+		}
+	}()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalln(err)
+	}
+
+	// grpc server
+	key := os.Getenv("SIGNING_KEY")
+	s := grpc.NewServer()
+	pb.RegisterAuthServiceServer(s, NewServer(db, key))
+
+	// listen and serve
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalln("Error listening to port 9000: ", err)
+	}
+
+	log.Println("Listening to port 9000.....")
+	log.Fatalln("Program terminated: ", s.Serve(lis))
+}
