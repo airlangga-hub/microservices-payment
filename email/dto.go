1371902662e54ee2e0ba98f8dfda9b41123a4fea
Airlangga <airlanggakrishna@gmail.com>
2026-01-05 10:52:41 +0700

    chore: email microsvc

---
+++ email/dto.go
@@ -0,0 +1,6 @@
+package main
+
+type EmailMessage struct {
+	OrderID string `json:"order_id"`
+	UserID  string `json:"user_id"`
+}
--- email/main.go
+++ email/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"log"
+
+	"github.com/IBM/sarama"
+)
+
 const (
 	topic = "email"
 )
+
+func main() {
+
+	consumer, err := sarama.NewConsumer()
+	if err != nil {
+		log.Fatalln("ERROR creating email consumer: ", err)
+	}
+
+}
