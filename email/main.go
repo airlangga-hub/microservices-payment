d1d4117e5db0c3faee096857d640bfbf64b5e296
Airlangga <airlanggakrishna@gmail.com>
2026-01-05 11:02:17 +0700

    chore

--- email/main.go
+++ email/main.go
@@ -25,4 +25,21 @@ func main() {
 		}
 	}()
 
+	partitions, err := consumer.Partitions(topic)
+	if err != nil {
+		log.Fatalln("ERROR getting email partitions: ", err)
+	}
+
+	for _, partition := range partitions {
+		partitionConsumer, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
+		if err != nil {
+			log.Fatalln("ERROR consuming email partition: ", err)
+		}
+
+		defer func() {
+			if err := partitionConsumer.Close(); err != nil {
+				log.Println("ERROR closing email partition consumer: ", err)
+			}
+		}()
+	}
 }
