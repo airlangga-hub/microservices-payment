69c48e4f8c54b369e90a1dca2e8848e8c0360c0d
Airlangga <airlanggakrishna@gmail.com>
2026-01-05 10:55:26 +0700

    chore

--- email/main.go
+++ email/main.go
@@ -11,10 +11,18 @@ const (
 )
 
 func main() {
+	done := make(chan struct{})
 
-	consumer, err := sarama.NewConsumer()
+	consumer, err := sarama.NewConsumer([]string{"kafka:9092"}, sarama.NewConfig())
 	if err != nil {
 		log.Fatalln("ERROR creating email consumer: ", err)
 	}
+
+	defer func() {
+		close(done)
+		if err := consumer.Close(); err != nil {
+			log.Println("ERROR closing email consumer")
+		}
+	}()
 
 }
