a85a4547191f150bb432e83847b4eeaebb84d865
Airlangga <airlanggakrishna@gmail.com>
2026-01-05 09:15:27 +0700

    chore: mod tidy for sarama

--- money_movement/publisher/publisher.go
+++ money_movement/publisher/publisher.go
@@ -1,8 +1,16 @@
 package publisher
 
+import (
+	"github.com/IBM/sarama"
+)
+
 const (
 	emailTopic  = "email"
 	ledgerTopic = "ledger"
 )
 
-func SendCaptureMessage(pid, srcUserID string, amount int32)
+func SendCaptureMessage(pid, srcUserID string, amount int32) {
+
+	// create publisher
+	publisher, err := sarama.NewSyncProducer()
+}
