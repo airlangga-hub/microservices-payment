2313ac023a74026c657fc877433dd095172a3a3a
Airlangga <airlanggakrishna@gmail.com>
2026-01-06 08:30:11 +0700

    chore

--- ledger/main.go
+++ ledger/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"sync"
+
+	"github.com/IBM/sarama"
 )
 
 const (
@@ -14,8 +16,8 @@ const (
 	dbPassword = "password"
 
 	dbName = "ledger"
-	
-	topic = "ledger "
+
+	topic = "ledger"
 )
 
 var wg sync.WaitGroup
@@ -37,4 +39,42 @@ func main() {
 	if err := db.Ping(); err != nil {
 		log.Fatalln(err)
 	}
+
+	done := make(chan struct{})
+
+	consumer, err := sarama.NewConsumer([]string{"kafka:9092"}, sarama.NewConfig())
+	if err != nil {
+		log.Fatalln("ERROR creating ledger consumer: ", err)
+	}
+
+	defer func() {
+		close(done)
+		if err := consumer.Close(); err != nil {
+			log.Println("ERROR closing ledger consumer")
+		}
+	}()
+
+	partitions, err := consumer.Partitions(topic)
+	if err != nil {
+		log.Fatalln("ERROR getting ledger partitions: ", err)
+	}
+
+	for _, partition := range partitions {
+		partitionConsumer, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
+		if err != nil {
+			log.Fatalln("ERROR consuming ledger partition: ", err)
+		}
+
+		defer func() {
+			if err := partitionConsumer.Close(); err != nil {
+				log.Println("ERROR closing ledger partition consumer: ", err)
+			}
+		}()
+
+		wg.Add(1)
+
+		go AwaitMessages(partitionConsumer, partition, done)
+	}
+
+	wg.Wait()
 }
