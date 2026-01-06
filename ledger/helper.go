1030bcb2b10bab5e24a7b7decb2977e90a071d9c
Airlangga <airlanggakrishna@gmail.com>
2026-01-06 08:43:05 +0700

    chore

---
+++ ledger/helper.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
+)
+
+func AwaitMessages(partitionConsumer sarama.PartitionConsumer, partition int32, done chan struct{}) {
+
+	defer wg.Done()
+
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			fmt.Printf("Partition %d - Received message %s\n", partition, string(msg.Value))
+			HandleMessage(msg)
+		case <-done:
+			fmt.Println("Done signal received, exiting go routine.....")
+			return
+		}
+	}
+}
+
+func HandleMessage(msg *sarama.ConsumerMessage) {
+
+	var ledger LedgerMessage
+
+	if err := json.Unmarshal(msg.Value, &ledger); err != nil {
+		log.Println("INFO error handling message (Unmarshal): ", err)
+		return
+	}
+
+	if err := Insert(ledger); err != nil {
+		log.Println("INFO error handling message (Insert): ", err)
+		return
+	}
+}
+
+func Insert(ledger LedgerMessage) error {
+
+	return nil
+}
